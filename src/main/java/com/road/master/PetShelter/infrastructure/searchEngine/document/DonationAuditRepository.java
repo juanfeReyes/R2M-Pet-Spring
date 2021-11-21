9d18f1258b748193bc9e3cb6531e65850050bd3f
juanfeReyes <juan.felipe.reyesg@gmail.com>
2021-11-20 22:22:51 -0500

    remove donation domain and infra

--- src/main/java/com/road/master/PetShelter/infrastructure/searchEngine/document/DonationAuditRepository.java
+++
@@ -1,6 +0,0 @@
-package com.road.master.PetShelter.infrastructure.searchEngine.document;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface DonationAuditRepository extends ElasticsearchRepository<DonationAuditDocument, String> {
-}

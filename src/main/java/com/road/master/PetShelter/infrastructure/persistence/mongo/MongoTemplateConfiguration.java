2d3880d70dd1b13ada408ca1ae9f2a247940af69
juanfeReyes <92064684+juanfeReyes@users.noreply.github.com>
2021-11-21 10:50:59 -0500

    Feat/add adoption (#6)
    refactor(donation): delete donation domain

--- src/main/java/com/road/master/PetShelter/infrastructure/persistence/mongo/MongoTemplateConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.road.master.PetShelter.infrastructure.persistence.mongo;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@Configuration
-@EnableMongoRepositories
-public class MongoTemplateConfiguration {
-}
